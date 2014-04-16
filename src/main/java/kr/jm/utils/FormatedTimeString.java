6e34a5438e2d7c67c9fcae586ea1bbd50513487b
jemin <hjm1980@gmail.com>
2014-04-16 17:05:21 +0900

    fix class name

--- src/main/java/kr/jm/utils/FormatedTimeString.java
+++ src/main/java/kr/jm/utils/FormatedTimeString.java
@@ -4,7 +4,7 @@ import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.TimeZone;
 
-public class FomatedTimeString {
+public class FormatedTimeString {
 
 	public static final String LONG_FORMAT_WITH_PLUS_TIMEZONE = "yyyy-MM-dd'T'HH:mm:ssZ"; //2014-03-21T18:31:23+0900
 	public static final String LONG_FORMAT_WITH_TIMEZONE = "yyyy-MM-dd'T'HH:mm:ssz"; //2014-03-21T18:31:23KST
@@ -13,11 +13,11 @@ public class FomatedTimeString {
 	public static final String SHORT_FORMAT_WITH_TIMEZONE = "yyyyMMddHHmmssz"; //20140321183123KST
 	public static final String SHORT_FORMAT_WITHOUT_TIMEZONE = "yyyyMMddHHmm"; //201403211831
 	 
-	private static final long aSecond = 1000;
-	private static final long aMinute = 60 * aSecond;
-	private static final long anHour = 60 * aMinute;
-	private static final long aDay = 24 * anHour;
-	private static final long aWeek = 7 * aDay;
+	public static final long aSecond = 1000;
+	public static final long aMinute = 60 * aSecond;
+	public static final long anHour = 60 * aMinute;
+	public static final long aDay = 24 * anHour;
+	public static final long aWeek = 7 * aDay;
 
 	public static String getTimeInUTC(long timestemp, String timeFormat) {
 		return getTime(timestemp, timeFormat, "UTC");
@@ -31,11 +31,15 @@ public class FomatedTimeString {
 		return timeformat.format(ts);
 	}
 
-	public static long getTimestampMinusParemeters(long beforeTimestemp,
+	public static long getTimestampMinusParemeters(long targetTimestemp,
 			int numOfWeeks, int numOfDays, int numOfHours, int numOfMinutes, int numOfSeconds) {
 		long sumOfParemeters = numOfWeeks * aWeek + numOfDays * aDay + numOfHours * anHour
 				+ numOfMinutes * aMinute + numOfSeconds * aSecond;
-		return beforeTimestemp - sumOfParemeters;
+		return targetTimestemp - sumOfParemeters;
+	}
+	
+	public static long getTimestampMinusTimes(long targetTimestemp, long times) {
+		return targetTimestemp - times;
 	}
 	
 	public static long getCurrentTimestampMinusParemeters(int numOfWeeks, int numOfDays, int numOfHours,
@@ -43,24 +47,24 @@ public class FomatedTimeString {
 		return getTimestampMinusParemeters(System.currentTimeMillis(), numOfWeeks, numOfDays, numOfHours, numOfMinutes, numOfSeconds);
 	}
 	
-	public static long getTimestampMinusSeconds(long beforeTimestemp, int numOfSeconds) {
-		return beforeTimestemp - numOfSeconds * aSecond;
+	public static long getTimestampMinusSeconds(long targetTimestemp, int numOfSeconds) {
+		return targetTimestemp - numOfSeconds * aSecond;
 	}
 	
-	public static long getTimestampMinusMinutes(long beforeTimestemp, int numOfMinutes) {
-		return beforeTimestemp - numOfMinutes * aMinute;
+	public static long getTimestampMinusMinutes(long targetTimestemp, int numOfMinutes) {
+		return targetTimestemp - numOfMinutes * aMinute;
 	}
 	
-	public static long getTimestampMinusHours(long beforeTimestemp, int numOfHours) {
-		return beforeTimestemp - numOfHours * anHour;
+	public static long getTimestampMinusHours(long targetTimestemp, int numOfHours) {
+		return targetTimestemp - numOfHours * anHour;
 	}
 	
-	public static long getTimestampMinusDays(long beforeTimestemp, int numOfDays) {
-		return beforeTimestemp - numOfDays * aDay;
+	public static long getTimestampMinusDays(long targetTimestemp, int numOfDays) {
+		return targetTimestemp - numOfDays * aDay;
 	}
 	
-	public static long getTimestampMinusWeeks(long beforeTimestemp, int numOfWeeks) {
-		return beforeTimestemp - numOfWeeks * aWeek;
+	public static long getTimestampMinusWeeks(long targetTimestemp, int numOfWeeks) {
+		return targetTimestemp - numOfWeeks * aWeek;
 	}	
 
 }
--- src/test/java/kr/jm/utils/FomatedTimeStringTest.java
+++ src/test/java/kr/jm/utils/FomatedTimeStringTest.java
@@ -9,7 +9,7 @@ private final long timestamp = 1395394283524l;
 	@Test
 	public void testFomatedTimeString() {		
 
-		FomatedTimeString fts = new FomatedTimeString();
+		FormatedTimeString fts = new FormatedTimeString();
 		System.out.println(fts.getTime(timestamp, fts.LONG_FORMAT_WITH_PLUS_TIMEZONE, "Asia/Seoul"));
 		System.out.println(fts.getTime(timestamp, fts.LONG_FORMAT_WITH_TIMEZONE, "Asia/Seoul"));
 		System.out.println(fts.getTime(timestamp, fts.LONG_FORMAT_WITHOUT_TIMEZONE, "Asia/Seoul"));
